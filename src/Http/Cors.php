a8ae5693eda8cdda8c36bbcc0320e7ff452db851
Daniel Pedro <danielfpedro@gmail.com>
2014-12-27 17:29:05 -0200

    muitas coisas

---
+++ src/Http/Cors.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mayhem\Http;
+
+/**
+ * Set the CORS to the app can be accessed from external origin
+ */
+class Cors
+{
+	/**
+	 * Set the CORS headers
+	 */
+	static public function setCORS()
+	{
+
+		if (isset($_SERVER['HTTP_ORIGIN'])) {
+		    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+		    header('Access-Control-Allow-Credentials: true');
+		    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+		    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		    header("Access-Control-Allow-Headers', 'Authorization, X-Authorization, Origin, Accept, Content-Type, X-Requested-With, X-HTTP-Method-Override");
+		}
+		if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+		    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])){
+		        header("Access-Control-Allow-Methods: GET, PUT, DELETE, OPTIONS");
+		    }
+		    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])){
+		        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+		    }
+		    exit(0);
+		}
+
+	}
+}
+
+?>
---
+++ src/Routing/Dispatcher.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Mayhem\Routing;
+
+use Slim\Slim;
+
+/**
+ * Dispatcher
+ */
+class Dispatcher
+{	
+	/**
+	 * Dispatch the rount geted from Slim framework
+	 * @param  array $config App config
+	 * @return method         The method called
+	 */
+	public static function dispatch($config)
+	{
+		$slim = new Slim(['debug'=> $config['debug']]);
+		$slim->map('/:controller(/:action)(/:params+)', function($controller, $action = null, $params = []) use ($slim, $config){
+			
+			$controller_class_name = Dispatcher::setControllerClassName($controller);
+
+			if (class_exists($controller_class_name)) {
+				$obj = new $controller_class_name;
+				$action = Dispatcher::resolveAction($action, $slim->request->getMethod());
+
+				if (method_exists($obj, $action)) {
+					$obj->request = $slim->request;
+					if ($config['responseType'] == 'JSON') {
+						echo json_encode(call_user_func_array([$obj, $action], $params));
+						return true;
+					}
+				} else {
+					$notFoundMsg = 'Action not Found';
+				}
+			} else {
+				$notFoundMsg = 'Controller not Found';
+			}
+
+			$slim->halt(404, ($config['debug']) ? $notFoundMsg : 'Not Found');
+		})
+		->via('GET', 'POST', 'PUT', 'DELETE');
+
+		$slim->run();
+	}
+
+/**
+ * Resolve the action name based on RESTful logic
+ * @param  string $action the action name to be resolved
+ * @param  string $method the method to say how the action will be resolved
+ * @return string
+ */
+	public static function resolveAction($action, $method)
+	{
+		if (!$action) {
+			switch ($method) {
+				case 'GET':
+					$action = "index";
+					break;
+				case 'POST':
+					$action = "add";
+					break;
+			}
+		} else {
+			if(is_numeric($action)){
+				switch ($method) {
+					case 'GET':
+						$action = "view";
+						break;
+					case 'PUT':
+						$action = "edit";
+						break;
+					case 'DELETE':
+						$action = "delete";
+						break;
+				}
+			}
+		}
+
+		return $action;
+	}
+
+/**
+ * Se the controller class name from a controller name
+ * @param  string $controller Controller name
+ * @return string             Controller class name
+ */
+	public static function setControllerClassName($controller)
+	{
+		return'App\Controller\\' . ucfirst($controller) . 'Controller';
+	}
+}
+
+?>

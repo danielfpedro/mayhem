0e1f343624bcfb57994431b06c1109d1ea4735a1
Daniel Pedro <danielfpedro@gmail.com>
2015-01-07 00:02:48 -0200

    Fazendo o model ainda

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -5,23 +5,43 @@ namespace Mayhem\Model;
 use Aura\SqlQuery\QueryFactory;
 use Mayhem\Database\Datasource;
 use PDO;
+use Exception;
 
 class Model
 {
+
+	public $primaryKeyName = 'id';
 	public $select;
+	public $insert;
+
+	private $pdo;
+	private $sth;
+
+	public $queryFactory;
+
+	public $errors;
 
 	public function __construct()
 	{
+		$this->setPDO();
+
+		$this->queryFactory = new QueryFactory('mysql');
 		$this->newQuery();
 	}
 
 	public function newQuery()
 	{
-		$query_factory = new QueryFactory('mysql');
-		$query = $query_factory->newSelect();
+		$query = $this->queryFactory->newSelect();
 
 		$query->from($this->tableName);
 		$this->select = $query;
+	}
+	public function newInsert()
+	{
+		$insert = $this->queryFactory->newInsert();
+
+		$insert->into($this->tableName);
+		$this->insert = $insert;
 	}
 
 	public static function connect($default = 'default')
@@ -36,12 +56,41 @@ class Model
 		}
 	}
 
-	private function executeQuery($query, $connection = null)
+	private function setPDO()
 	{
-		$pdo = self::connect();
-		$sth = $pdo->prepare($query->__toString());
-		$sth->execute($query->getBindValues());
+		$this->pdo = self::connect();
+	}
 
+	public function save($data)
+	{
+		$insert = $this->queryFactory->newInsert();
+		$insert->into($this->tableName)->cols($data);
+		$this->executeQuery($insert);
+	}
+
+	public function update($data)
+	{
+		if (array_key_exists($this->primaryKeyName, $data)) {
+			$id = $data[$this->primaryKeyName];
+			unset($data[$this->primaryKeyName]);
+			$update = $this->queryFactory->newUpdate();
+			$update
+				->table($this->tableName)
+				->cols($data)
+				->where("{$this->primaryKeyName} = :id")
+				->bindValue($this->primaryKeyName, $id);
+			return $this->executeQuery($update);
+		} else {
+			throw new Exception("Can't update, primary key needed", 1);
+		}
+
+		return true;
+	}
+
+	public function executeQuery($query, $connection = null)
+	{
+		$sth = $this->pdo->prepare($query->__toString());
+		$sth->execute($query->getBindValues());
 		return $sth;
 	}
 

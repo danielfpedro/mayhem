b542019fc19444e6e0a2df95680e8cd796550e7d
Daniel Pedro <ag.interagir@gmail.com>
2015-01-29 12:12:27 -0200

    Arrumei Umas coisas do CORS

--- src/Http/Cors.php
+++ src/Http/Cors.php
@@ -16,9 +16,8 @@ class Cors
 		if (isset($_SERVER['HTTP_ORIGIN'])) {
 		    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
 		    header('Access-Control-Allow-Credentials: true');
-		    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+		    header('Access-Control-Max-Age: 86400');
 		    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD");
-		    header("Access-Control-Allow-Headers', 'Authorization, X-Authorization, Origin, Accept, Content-Type, X-Requested-With, X-HTTP-Method-Override");
 		}
 		if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 		    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])){
